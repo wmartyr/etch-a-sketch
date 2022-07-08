b748233bf3d24fd1d92280a37d97e79269c8e655
Woodrow Martyr <woodrow.martyr@gmail.com>
2022-07-08 22:13:16 +0930

    Code the grid using javascript

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,20 @@
+let side = 16;
+
 const mainGrid = document.querySelector("#main-grid");
-for (let i = 0; i < 2; i++) {
+for (let i = 0; i < side; i++) {
   const rowDiv = document.createElement("div");
-  rowDiv.classList.add(`row%{i}`);
+  rowDiv.setAttribute("id", `row%{i}`);
+  rowDiv.classList.add("row-setup");
   mainGrid.appendChild(rowDiv);
 
-  for (let j = 0; j < 2; j++) {
+  for (let j = 0; j < side; j++) {
     const div = document.createElement("div");
-    div.textContent = `div ${j}`;
+    div.classList.add("cell");
+    div.style["width"] = `calc(800px / ${side})`;
+    div.style["height"] = `calc(800px / ${side})`;
+    div.textContent = `r: ${i}, c: ${j}`;
     rowDiv.appendChild(div);
 
   }
 }
 
-// const div = document.createElement("div");
-// div.textContent = "div test";
-// mainGrid.appendChild(div);
