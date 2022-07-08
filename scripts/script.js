74c4a9a4ea0310f4b1357b8939269b0abffafadb
Woodrow Martyr <woodrow.martyr@gmail.com>
2022-07-08 22:26:56 +0930

    Add id to all cells

--- scripts/script.js
+++ scripts/script.js
@@ -3,16 +3,17 @@ let side = 16;
 const mainGrid = document.querySelector("#main-grid");
 for (let i = 0; i < side; i++) {
   const rowDiv = document.createElement("div");
-  rowDiv.setAttribute("id", `row%{i}`);
+  rowDiv.setAttribute("id", `row${i}`);
   rowDiv.classList.add("row-setup");
   mainGrid.appendChild(rowDiv);
 
   for (let j = 0; j < side; j++) {
     const div = document.createElement("div");
     div.classList.add("cell");
+    div.setAttribute("id", `row${i}-cell${j}`);
     div.style["width"] = `calc(800px / ${side})`;
     div.style["height"] = `calc(800px / ${side})`;
-    div.textContent = `r: ${i}, c: ${j}`;
+    // div.textContent = `r: ${i}, c: ${j}`;
     rowDiv.appendChild(div);
 
   }
